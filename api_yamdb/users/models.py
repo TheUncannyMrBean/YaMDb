331fb66443b7719b340c274817c75948aae2b311
TheUncannyMrBean <theuncannymrbean@pop-os.localdomain>
2023-04-07 17:10:52 +0700

    First steps

---
+++ api_yamdb/users/models.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class UserRole(models.Model):
+
+    USERROLE = (
+        ('USER', 'user'),
+        ('MODERATOR', 'moderator'),
+        ('ADMIN', 'admin')
+    )
+
+
+class User(AbstractUser):
+    bio = models.TextField(
+        blank=True, verbose_name='О себе'
+    )
+    role = models.CharField(
+        choices=UserRole.USERROLE,
+        default='USER',
+        max_length=40,
+        verbose_name='Роль пользователя'
+    )
+    email = models.EmailField(
+        unique=True,
+        blank=False,
+        max_length=254,
+        verbose_name='Электронная почта'
+    )
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        ordering = ['username']
